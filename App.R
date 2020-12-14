dfc0a8638d566c11fbb41cd0d7074a33c2c05134
HungKNguyen <75971367+HungKNguyen@users.noreply.github.com>
2020-12-14 09:01:49 -0600

    1. Implement Interactive Plot 2. Adjust c, d into slider input alpha

--- App.R
+++ App.R
@@ -2,10 +2,11 @@ library(Rsolnp)
 library(tidyverse)
 library(ggplot2)
 library(shiny)
+library(plotly)
 
 #Function construction
-setting <- function (c, d, Px, Py, I, seed_x, seed_y, hick, newPx, newI) {
-  list("c" = c,"d" = d,"Px" = c(Px, newPx),"Py" = Py,"I" = c(I, newI),"seed" = c(seed_x,seed_y),"hick" = hick)
+setting <- function (a, Px, Py, I, seed_x, seed_y, hick, newPx, newI) {
+  list("c" = a,"d" = 1-a,"Px" = c(Px, newPx),"Py" = Py,"I" = c(I, newI),"seed" = c(seed_x,seed_y),"hick" = hick)
 }
 
 consummer_optimization <- function(setting) {
@@ -74,20 +75,25 @@ graphing <- function(solution, setting) {
   
   ultility = function(x){(u/(x^(setting$c[1])))^(1/(setting$d[1]))}
   
-  if(setting$hick[1] == "No") { 
+  if(setting$hick[1] == "No") {
     
-    coords = sprintf("(%s , %s)", format(round(solution[1], digits = 3)), format(round(solution[2], digits = 3)))
+    #Create dataframe
+    x <- seq(from = 0, to = setting$I[1]/setting$Px[1]*1.2, length.out = 10000)
+    budgetline <- budget(x)
+    indiffcurve <- ultility(x)
+    data <- data.frame(x, budgetline, indiffcurve)
     
-    print(ggplot(data.frame(x=c(0,setting$I[1]/setting$Px[1]*1.2)), aes(x)) + 
-            stat_function(fun=budget, color = "red", size = 1) +
-            stat_function(fun=ultility, color = "blue", size = 1) +
-            ylim(0,setting$I[1]/setting$Py[1]*1.2) +
-            geom_point(x=c(solution[1]), y =c(solution[2]), size =2) +
-            geom_label(aes(solution[1], solution[2]-(setting$I[1]/setting$Py[1]*0.1), label=coords), alpha = 0.2) +
-            theme_minimal() +
-            ggtitle("Visual for the Solution")) +
-      xlab("Quantity of product X") +
-      ylab("Qunatity of product Y")
+    # Base plot
+    fig <- plot_ly(data, x = ~x) 
+    fig <- fig %>% add_trace(y = ~budgetline, name = 'Budget Constaint',mode = 'lines')
+    fig <- fig %>% add_trace(y = ~indiffcurve, name = 'Indifference Curve',mode = 'lines')
+    fig <- fig %>% add_trace(x = ~c(round(solution[1], digits =3)), y = ~c(round(solution[2], digits =3)), name = "Optimal Bundle", type = 'scatter', mode = 'markers')
+    fig <- fig %>% layout(
+      yaxis = list(title = "Quantity of Product Y", range = c(0,setting$I[1]/setting$Py[1]*1.2 )),
+      legend = list(x = 0.9, y = 0.9),
+      xaxis= list(title = "Quantity of Product X"),
+      title = "Graphical Solution for Consumer Optimization")
+    
   } else {
     newU = solution[3]^setting$c[1]*solution[4]^setting$d[1]
     
@@ -95,64 +101,66 @@ graphing <- function(solution, setting) {
     
     newUltility = function(x){(newU/(x^(setting$c[1])))^(1/(setting$d[1]))}
     
-    hick_Income = solution[6]*setting$Py[1] + solution[5]*setting$Px[2]
+    hickIncome = solution[6]*setting$Py[1] + solution[5]*setting$Px[2]
     
-    hickBudget= function(x) {-(setting$Px[2]*x-hick_Income)/setting$Py[1]}
+    hickBudget= function(x) {-(setting$Px[2]*x-hickIncome)/setting$Py[1]}
     
-    coords_sol = sprintf("(%s , %s)", format(round(solution[1], digits = 3)), format(round(solution[2], digits = 3)))
-    coords_new = sprintf("(%s , %s)", format(round(solution[3], digits = 3)), format(round(solution[4], digits = 3)))
-    coords_hick = sprintf("(%s , %s)", format(round(solution[5], digits = 3)), format(round(solution[6], digits = 3)))
+    x_intercept <- max(c(setting$I[1]/setting$Px[1], setting$I[2]/setting$Px[2], solution[5]))*1.2
+    y_intercept <- max(c(setting$I[1]/setting$Py[1], setting$I[2]/setting$Py[1], solution[6]))*1.2
     
-    x_intercept <- max(c(setting$I[1]/setting$Px[1]*1.2, setting$I[2]/setting$Px[2]*1.5))
-    y_intercept <- max(c(setting$I[1]/setting$Py[1]*1.2, setting$I[2]/setting$Py*1.5))
+    #Create dataframe
+    x <- seq(from = 0, to = x_intercept, length.out = 10000)
+    budgetline <- budget(x)
+    budgetline2 <- newBudget(x)
+    indiffcurve <- ultility(x)
+    indiffcurve2 <- newUltility(x)
+    hicksline <- hickBudget(x)
+    data <- data.frame(x, budgetline, budgetline2, indiffcurve, indiffcurve2, hicksline)
     
-    print(ggplot(data.frame(x=c(0,x_intercept)), aes(x)) + 
-            stat_function(fun=budget, color = "red", size = 1) +
-            stat_function(fun=newBudget, color = "orange", size = 1) +
-            stat_function(fun=ultility, color = "blue", size = 1) +
-            stat_function(fun=newUltility, color = "green", size = 1) +
-            stat_function(fun=hickBudget, color = "black", size = 1) +
-            ylim(0,y_intercept) +
-            geom_point(x=c(solution[1]), y =c(solution[2]), size =2) +
-            geom_point(x=c(solution[3]), y =c(solution[4]), size =2) +
-            geom_point(x=c(solution[5]), y =c(solution[6]), size =2) +
-            geom_label(aes(solution[1], solution[2]-(y_intercept*0.07), label=coords_sol), alpha = 0.2) +
-            geom_label(aes(solution[3], solution[4]-(y_intercept*0.07), label=coords_new), alpha = 0.2) +
-            geom_label(aes(solution[5], solution[6]-(y_intercept*0.07), label=coords_hick), alpha = 0.2) +
-            theme_minimal() +
-            ggtitle("Visual for the Solution")) +
-      xlab("Quantity of product X") +
-      ylab("Qunatity of product Y")
+    # Base plot
+    fig <- plot_ly(data, x = ~x)
+    fig <- fig %>% add_trace(y = ~budgetline, name = 'Budget Constaint',mode = 'lines')
+    fig <- fig %>% add_trace(y = ~budgetline2, name = 'New Budget Constaint',mode = 'lines')
+    fig <- fig %>% add_trace(y = ~indiffcurve, name = 'Indifference Curve',mode = 'lines')
+    fig <- fig %>% add_trace(y = ~indiffcurve2, name = 'New Indifference Curve',mode = 'lines')
+    fig <- fig %>% add_trace(y = ~hicksline, name = 'Hicksian Compensated Demand',mode = 'lines')
+    fig <- fig %>% add_trace(x = ~c(round(solution[1], digits =3)), y = ~c(round(solution[2], digits =3)), name = "Optimal Bundle", type = 'scatter', mode = 'markers')
+    fig <- fig %>% add_trace(x = ~c(round(solution[3], digits =3)), y = ~c(round(solution[4], digits =3)), name = "New Optimal Bundle", type = 'scatter', mode = 'markers')
+    fig <- fig %>% add_trace(x = ~c(round(solution[5], digits =3)), y = ~c(round(solution[6], digits =3)), name = "Hicksian Decomposition", type = 'scatter', mode = 'markers')
+    fig <- fig %>% layout(
+      yaxis = list(title = "Quantity of Product Y", range = c(0,y_intercept)),
+      legend = list(x = 0.9, y = 0.9),
+      xaxis= list(title = "Quantity of Product X"),
+      title = "Graphical Solution for Consumer Optimization")
   }
 }
 
 # R shiny
-{
-ui <- fluidPage(
-  numericInput("c", "Value for c:", "", min = 0, width = "10%"),
-  numericInput("d", "Value for d:", "", min = 0, width = "10%"),
-  numericInput("Px", "Price of x:", "", min = 0, width = "10%"),
-  numericInput("Py", "Price of y:", "", min = 0, width = "10%"),
-  numericInput("I", "Consumer's Income:", "", min = 0, width = "10%"),
-  numericInput("seed_x", "Seed value for x: ", 1, min = 0, width = "10%"),
-  numericInput("seed_y", "Seed value for y: ", 1, min = 0, width = "10%"),
+
+ui <- {fluidPage(
+  sliderInput("a", "Alpha Parameter:", min = 0, max =1, value = 0.5, step =0.005),
+  numericInput("Px", "Price of x:", "", min = 0),
+  numericInput("Py", "Price of y:", "", min = 0),
+  numericInput("I", "Consumer's Income:", "", min = 0),
+  numericInput("seed_x", "Seed value for x: ", 1, min = 0),
+  numericInput("seed_y", "Seed value for y: ", 1, min = 0),
   
   selectInput("hick", "Draw Hicksian compensated demand (Optional):",
               c("No" = "No",
                 "Yes" = "Yes")),
-  numericInput("newPx", "Price of x for compensated demand:", 0, min = 0, width = "10%"),
-  numericInput("newI", "Consumer's Income for compensated demand:", 0, min = 0, width = "10%"),
+  numericInput("newPx", "Price of x for compensated demand:", 0, min = 0),
+  numericInput("newI", "Consumer's Income for compensated demand:", 0, min = 0),
   
   actionButton("button", "Run"),
   
-  plotOutput("graph"),
+  plotlyOutput("graph"),
   textOutput("result")
-)
+)}
 
 server <- function(input, output){
   
   settingA <- eventReactive(input$button,{ 
-    setting(input$c,input$d,input$Px,input$Py,input$I,input$seed_x,input$seed_y,input$hick,input$newPx,input$newI)
+    setting(input$a,input$Px,input$Py,input$I,input$seed_x,input$seed_y,input$hick,input$newPx,input$newI)
   })
   
   A <- eventReactive(input$button,{
@@ -165,8 +173,7 @@ server <- function(input, output){
   
   output$result <- renderText({text()})
   
-  output$graph <- renderPlot({graph()})
+  output$graph <- renderPlotly({graph()})
 }
 
 shinyApp(ui = ui, server = server)
-}
