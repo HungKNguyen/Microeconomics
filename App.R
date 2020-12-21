3096efd86bb79032c06b424d72c1aba2b05262f2
HungKNguyen <75971367+HungKNguyen@users.noreply.github.com>
2020-12-21 06:11:35 -0600

    Bug fixing Implemented advance option Implemented changing P'y

--- App.R
+++ App.R
@@ -3,8 +3,8 @@ library(plotly)
 
 #Function construction
 
-setting <- function(a, Px, Py, I, hick, newPx, newI) {
-  list("c" = a,"d" = 1-a,"Px" = c(Px, newPx),"Py" = Py,"I" = c(I, newI),"hick" = hick)
+setting <- function(a, Px, Py, I, hick, newI, newPx, newPy) {
+  list("c" = a,"d" = 1-a,"Px" = c(Px, newPx),"Py" = c(Py, newPy),"I" = c(I, newI),"hick" = hick)
 }
 
 consummer_optimization <- function(setting) {
@@ -29,17 +29,19 @@ consummer_optimization <- function(setting) {
     "Hicksian" = {
       
        newX_sol = (setting$I[2]*setting$c[1])/setting$Px[2]
-       newY_sol = (setting$I[2]*setting$d[1])/setting$Py[1]
+       newY_sol = (setting$I[2]*setting$d[1])/setting$Py[2]
        
        u = x_sol^setting$c[1]*y_sol^setting$d[1]
        
-       alpha = (setting$Px[2] * setting$d[1])/(setting$Py[1] * setting$c[1])
+       alpha = (setting$Px[2] * setting$d[1])/(setting$Py[2] * setting$c[1])
        
        hick_x = (u/(alpha^setting$d[1]))^(1/(setting$d[1]+setting$c[1]))
        hick_y = alpha * hick_x
        
        text <- HTML(sprintf(
-         "<h4>Problem:</h4> Maximize &nbsp; \\(u = x^{%s}y^{%s}\\) &nbsp; &nbsp; with constraint &nbsp; \\(%s\\cdot x + %s\\cdot y = %s\\). <br/> <br/> 
+         "<h4>Problem:</h4> 
+         Maximize &nbsp; \\(u = x^{%s}y^{%s}\\) &nbsp; &nbsp; with constraint &nbsp; \\(%s\\cdot x + %s\\cdot y = %s\\). <br/>
+         New constraint of &nbsp; \\(%s\\cdot x + %s\\cdot y = %s\\). <br/> <br/> 
           <h4>Solution:</h4> 
           \\(x = %s \\qquad y = %s\\) <br/> 
           \\(x' = %s \\qquad y' = %s \\) <br/> 
@@ -49,12 +51,13 @@ consummer_optimization <- function(setting) {
           \\(Substitution \\ Effect \\ x = %s\\) <br/>
           \\(Income \\ Effect \\ y = %s\\) <br/>
           \\(Substitution \\ Effect \\ y = %s\\) <br/> <br/> <br/> <br/> <br/>",
-         setting$c[1], setting$d[1], setting$Px[1], setting$Py[1], setting$I[1], 
+         setting$c[1], setting$d[1], setting$Px[1], setting$Py[1], setting$I[1],
+         setting$Px[2], setting$Py[2], setting$I[2],
          round(x_sol, digits = 3), round(y_sol, digits = 3), round(newX_sol, digits = 3), round(newY_sol, digits = 3),
          round(hick_x, digits = 3), round(hick_y, digits = 3),
          round(newX_sol-hick_x, digits = 3),
-         round(newY_sol-hick_y, digits = 3),
          round(-x_sol+hick_x, digits = 3),
+         round(newY_sol-hick_y, digits = 3),
          round(-y_sol+hick_y, digits = 3)
        ))
        
@@ -65,15 +68,17 @@ consummer_optimization <- function(setting) {
     "Slutsky" ={
       
       newX_sol = (setting$I[2]*setting$c[1])/setting$Px[2]
-      newY_sol = (setting$I[2]*setting$d[1])/setting$Py[1]
+      newY_sol = (setting$I[2]*setting$d[1])/setting$Py[2]
       
-      slutskyI = x_sol*setting$Px[2] + y_sol*setting$Py[1]
+      slutskyI = x_sol*setting$Px[2] + y_sol*setting$Py[2]
       
       ssky_x = (slutskyI*setting$c[1])/setting$Px[2]
-      ssky_y = (slutskyI*setting$d[1])/setting$Py[1]
+      ssky_y = (slutskyI*setting$d[1])/setting$Py[2]
       
       text <- HTML(sprintf(
-        "<h4>Problem:</h4> Maximize &nbsp; \\(u = x^{%s}y^{%s}\\) &nbsp; &nbsp; with constraint &nbsp; \\(%s\\cdot x + %s\\cdot y = %s\\). <br/> <br/> 
+        "<h4>Problem:</h4> 
+        Maximize &nbsp; \\(u = x^{%s}y^{%s}\\) &nbsp; &nbsp; with constraint &nbsp; \\(%s\\cdot x + %s\\cdot y = %s\\). <br/> 
+        New constraint of &nbsp; \\(%s\\cdot x + %s\\cdot y = %s\\). <br/> <br/> 
           <h4>Solution:</h4> 
           \\(x = %s \\qquad y = %s\\) <br/> 
           \\(x' = %s \\qquad y' = %s \\) <br/> 
@@ -84,11 +89,12 @@ consummer_optimization <- function(setting) {
           \\(Income \\ Effect \\ y = %s\\) <br/>
           \\(Substitution \\ Effect \\ y = %s\\) <br/> <br/> <br/> <br/> <br/>",
         setting$c[1], setting$d[1], setting$Px[1], setting$Py[1], setting$I[1], 
+        setting$Px[2], setting$Py[2], setting$I[2],
         round(x_sol, digits = 3), round(y_sol, digits = 3), round(newX_sol, digits = 3), round(newY_sol, digits = 3),
         round(ssky_x, digits = 3), round(ssky_y, digits = 3),
         round(newX_sol-ssky_x, digits = 3),
-        round(newY_sol-ssky_y, digits = 3),
         round(-x_sol+ssky_x, digits = 3),
+        round(newY_sol-ssky_y, digits = 3),
         round(-y_sol+ssky_y, digits = 3)
       ))
       
@@ -108,13 +114,13 @@ graphing <- function(solution, setting, x_scale, y_scale) {
   if(setting$hick[1] == "Hicksian") {
     newU = solution[3]^setting$c[1]*solution[4]^setting$d[1]
     
-    newBudget= function(x) {-(setting$Px[2]*x-setting$I[2])/setting$Py[1]}
+    newBudget= function(x) {-(setting$Px[2]*x-setting$I[2])/setting$Py[2]}
     
     newUltility = function(x){(newU/(x^(setting$c[1])))^(1/(setting$d[1]))}
     
-    hickIncome = solution[6]*setting$Py[1] + solution[5]*setting$Px[2]
+    hickIncome = solution[6]*setting$Py[2] + solution[5]*setting$Px[2]
     
-    hickBudget= function(x) {-(setting$Px[2]*x-hickIncome)/setting$Py[1]}
+    hickBudget= function(x) {-(setting$Px[2]*x-hickIncome)/setting$Py[2]}
   }
   
   if(setting$hick[1] == "Slutsky") {
@@ -122,13 +128,13 @@ graphing <- function(solution, setting, x_scale, y_scale) {
     
     sskyU = solution[5]^setting$c[1]*solution[6]^setting$d[1]
     
-    newBudget= function(x) {-(setting$Px[2]*x-setting$I[2])/setting$Py[1]}
+    newBudget= function(x) {-(setting$Px[2]*x-setting$I[2])/setting$Py[2]}
     
     newUltility = function(x){(newU/(x^(setting$c[1])))^(1/(setting$d[1]))}
     
-    slutskyIncome = solution[5]*setting$Px[2] + solution[6]*setting$Py[1]
+    slutskyIncome = solution[5]*setting$Px[2] + solution[6]*setting$Py[2]
     
-    slutskyBudget= function(x) {-(setting$Px[2]*x-slutskyIncome)/setting$Py[1]}
+    slutskyBudget= function(x) {-(setting$Px[2]*x-slutskyIncome)/setting$Py[2]}
     
     sskyUltility = function(x){(sskyU/(x^(setting$c[1])))^(1/(setting$d[1]))}
   }
@@ -212,40 +218,69 @@ ui <- navbarPage("MicroEconomics by Luke and Hung",
                              
                              helpText(h4("Budget  \\(I=P_x\\cdot x+P_y\\cdot y\\)")),
                              
-                             
                              fluidRow(
-                               column(3,
-                                  numericInput(inputId = "minI", value = 0, label = "Min")
-                                ),
-                               column(6,
-                                  sliderInput("I", "I:",min = 0, max = 250, value = 250)    
+                               column(9,
+                                  sliderInput("I", "I:",min = 0, max = 250, value = 250, step = 250/200)    
                                 ),
                                column(3,
-                                  numericInput(inputId = "maxI", value = 250, label = "Max")   
+                                  checkboxInput("settingI", "Advance Setting")
                                 )
                              ),
-                            
-                             fluidRow(
-                               column(3,
-                                      numericInput(inputId = "minPx", value = 0, label = "Min \\(P_x\\)")
+                             
+                             conditionalPanel(
+                               condition = "input.settingI == true",
+                               column(4,
+                                      numericInput(inputId = "minI", value = 0, label = "Min I")
                                ),
-                               column(6,
-                                      sliderInput("Px", "\\(P_x\\):",min = 0, max = 20, value = 20)    
+                               column(4,
+                                      numericInput(inputId = "maxI", value = 250, label = "Max I")   
                                ),
-                               column(3,
-                                      numericInput(inputId = "maxPx", value = 20, label = "Max \\(P_x\\)")   
+                               column(4,
+                                      numericInput(inputId = "cusI", value = 250, label = "Value of I")   
                                )
                              ),
                              
                              fluidRow(
+                               column(9,
+                                      sliderInput("Px", "\\(P_x\\):",min = 0, max = 20, value = 20, step = 20/200)    
+                               ),
                                column(3,
+                                      checkboxInput("settingPx", "Advance Setting")
+                               )
+                             ),
+                             
+                             conditionalPanel(
+                               condition = "input.settingPx == true",
+                               column(4,
+                                      numericInput(inputId = "minPx", value = 0, label = "Min \\(P_x\\)")
+                               ),
+                               column(4,
+                                      numericInput(inputId = "maxPx", value = 20, label = "Max \\(P_x\\)")   
+                               ),
+                               column(4,
+                                      numericInput(inputId = "cusPx", value = 20, label = "Value of \\(P_x\\)")   
+                               )
+                             ),
+                             
+                             fluidRow(
+                               column(9,
+                                      sliderInput("Py", "\\(P_y\\):",min = 0, max = 20, value = 20, step = 20/200)   
+                               ),
+                               column(3,
+                                      checkboxInput("settingPy", "Advance Setting") 
+                               )
+                             ),
+                             
+                             conditionalPanel(
+                               condition = "input.settingPy == true",
+                               column(4,
                                       numericInput(inputId = "minPy", value = 0, label = "Min \\(P_y\\)")
                                ),
-                               column(6,
-                                      sliderInput("Py", "\\(P_y\\):",min = 0, max = 20, value = 20)   
+                               column(4,
+                                      numericInput(inputId = "maxPy", value = 20, label = "Max \\(P_y\\)")   
                                ),
-                               column(3,
-                                      numericInput(inputId = "maxPy", value = 20, label = "Max \\(P_y\\)") 
+                               column(4,
+                                      numericInput(inputId = "cusPy", value = 20, label = "Value of \\(P_y\\)")   
                                )
                              )
                           ),
@@ -258,28 +293,70 @@ ui <- navbarPage("MicroEconomics by Luke and Hung",
                              helpText(h4("New Budget  \\(I'=P'_x\\cdot x+P_y\\cdot y\\)")),
                              
                              fluidRow(
-                               column(3,
-                                      numericInput(inputId = "minNewPx", value = 0, label = "Min \\(P'_x\\)")
-                               ),
-                               column(6,
-                                      sliderInput("NewPx", "\\(P'_x\\):",min = 0, max = 40, value = 40)
+                               column(9,
+                                      sliderInput("NewI", "\\(I'\\):",min = 0, max = 250, value = 250, step = 250/200)  
                                ),
                                column(3,
-                                      numericInput(inputId = "maxNewPx", value = 40, label = "Max \\(P_x\\)")   
+                                      checkboxInput("settingNewI", "Advance Setting") 
+                               )
+                             ),
+                             
+                             conditionalPanel(
+                               condition = "input.settingNewI == true",
+                               column(4,
+                                      numericInput(inputId = "minNewI", value = 0, label = "Min I'")
+                               ),
+                               column(4,
+                                      numericInput(inputId = "maxNewI", value = 250, label = "Max I'")    
+                               ),
+                               column(4,
+                                      numericInput(inputId = "cusNewI", value = 250, label = "Value of I'")   
                                )
                              ),
                              
                              fluidRow(
-                               column(3,
-                                      numericInput(inputId = "minNewI", value = 0, label = "Min New I")
-                               ),
-                               column(6,
-                                      sliderInput("NewI", "\\(I'\\):",min = 0, max = 250, value = 40)  
+                               column(9,
+                                      sliderInput("NewPx", "\\(P'_x\\):",min = 0, max = 40, value = 40, step = 40/200)
                                ),
                                column(3,
-                                      numericInput(inputId = "maxNewI", value = 250, label = "Max New I")   
+                                      checkboxInput("settingNewPx", "Advance Setting")   
                                )
-                             )
+                             ),
+                             
+                             conditionalPanel(
+                               condition = "input.settingNewPx == true",
+                               column(4,
+                                      numericInput(inputId = "minNewPx", value = 0, label = "Min \\(P'_x\\)")
+                               ),
+                               column(4,
+                                      numericInput(inputId = "maxNewPx", value = 40, label = "Max \\(P'_x\\)")   
+                               ),
+                               column(4,
+                                      numericInput(inputId = "cusNewPx", value = 40, label = "Value of \\(P'_x\\)")   
+                               )
+                             ),
+                             
+                             fluidRow(
+                               column(9,
+                                      sliderInput("NewPy", "\\(P'_y\\):",min = 0, max = 20, value = 20, step = 20/200)
+                               ),
+                               column(3,
+                                      checkboxInput("settingNewPy", "Advance Setting")   
+                               )
+                             ),
+                             
+                             conditionalPanel(
+                               condition = "input.settingNewPy == true",
+                               column(4,
+                                      numericInput(inputId = "minNewPy", value = 0, label = "Min \\(P'_y\\)")
+                               ),
+                               column(4,
+                                      numericInput(inputId = "maxNewPy", value = 20, label = "Max \\(P'_y\\)")   
+                               ),
+                               column(4,
+                                      numericInput(inputId = "cusNewPy", value = 20, label = "Value of \\(P'_y\\)")   
+                               )
+                             ),
                           )
                         )
                       ),
@@ -306,33 +383,44 @@ server <- function(input, output, session) {
     
     minI <- input$minI
     maxI <- input$maxI
-    updateSliderInput(session, "I", value = maxI,
-                      min = minI, max = maxI, step = 1)
+    cusI <- input$cusI
+    updateSliderInput(session, "I", value = cusI,
+                      min = minI, max = maxI, step = (maxI - minI)/200,)
     
     minPx <- input$minPx
     maxPx <- input$maxPx
-    updateSliderInput(session, "Px", value = maxPx,
-                      min = minPx, max = maxPx, step = 1)
+    cusPx <- input$cusPx
+    updateSliderInput(session, "Px", value = cusPx,
+                      min = minPx, max = maxPx, step = (maxPx - minPx)/200,)
     
     minPy <- input$minPy
     maxPy <- input$maxPy
-    updateSliderInput(session, "Py", value = maxPy,
-                      min = minPy, max = maxPy, step = 1)
-    
-    minNewPx <- input$minNewPx
-    maxNewPx <- input$maxNewPx
-    updateSliderInput(session, "NewPx", value = maxNewPx,
-                      min = minNewPx, max = maxNewPx, step = 1)
+    cusPy <- input$cusPy
+    updateSliderInput(session, "Py", value = cusPy,
+                      min = minPy, max = maxPy, step = (maxPy - minPy)/200,)
     
     minNewI <- input$minNewI
     maxNewI <- input$maxNewI
+    cusNewI <- input$cusNewI
     updateSliderInput(session, "NewI", value = maxNewI,
-                      min = minNewI, max = maxNewI, step = 1)
+                      min = minNewI, max = maxNewI, step = (maxNewI - minNewI)/200,)
+    
+    minNewPx <- input$minNewPx
+    maxNewPx <- input$maxNewPx
+    cusNewPx <- input$cusNewPx
+    updateSliderInput(session, "NewPx", value = cusNewPx,
+                      min = minNewPx, max = maxNewPx, step = (maxNewPx - minNewPx)/200,)
+    
+    minNewPy <- input$minNewPy
+    maxNewPy <- input$maxNewPy
+    cusNewPy <- input$cusNewPy
+    updateSliderInput(session, "NewPy", value = cusNewPy,
+                      min = minNewPy, max = maxNewPy, step = (maxNewPy - minNewPy)/200,)
     
   })
   
   settingA <- reactive({ 
-    setting(input$alpha,input$Px,input$Py,input$I,input$hick,input$NewPx,input$NewI)
+    setting(input$alpha,input$Px,input$Py,input$I,input$hick,input$NewI,input$NewPx,input$NewPy)
   })
   
   A <- reactive({
@@ -348,8 +436,10 @@ server <- function(input, output, session) {
           input$I*12/input$NewPx, input$NewI*12/input$NewPx))/12
   })
   y_scale <- reactive({
-    max(c(input$maxI/(input$minPy+1), input$maxNewI/(input$minPy+1),
-          input$I*12/input$Py, input$NewI*12/input$Py))/12
+    max(c(input$maxI/(input$minPy+1), input$maxI/(input$minNewPy+1),
+          input$maxNewI/(input$minPy+1), input$maxNewI/(input$minNewPy+1),
+          input$I*12/input$Py, input$NewI*12/input$Py,
+          input$I*12/input$NewPy, input$NewI*12/input$NewPy))/12
   })
   graph <- reactive({ graphing(A()$solution, settingA(), x_scale(), y_scale()) })
   
